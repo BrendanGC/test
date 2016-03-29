41a76e5042e3ab6ce98236741183e74360ce7028
Brendan Sahli <brendan@gradconnection.com>
2016-03-29 12:58:03 +0200

    Test

---
+++ js/config.js
@@ -0,0 +1,15 @@
+configReactDriveCms = {
+    author: 'Brendan Sahli',
+    /**
+     * Add the Id for the Google Sheets Dashboard file
+     */
+    dashboardId: '1KhXZDmGMV68-X4p_rB_ClPUW7b2shbqDn0nZb1ewi30',
+    /**
+     * Add the Url Id for the Google Web App Script to forward email from the Contact Form
+     */
+    sendMailUrlId: 'https://drive.google.com/open?id=14cFxrjW2482ITPyZLxUlWrbrz-uK0MqoPsDx0AOFLrLQl7YA6a3536q4',
+    /**
+     * Add your Disqus shortname
+     */
+    shortname: 'Archy_za'
+};
